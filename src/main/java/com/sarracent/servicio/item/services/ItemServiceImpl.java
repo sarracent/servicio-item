76a5d54d53d57ebe86bdd5b1f250163d7d87abdf
Damian Sarracent <damian.sarracent@sondeos.com.ar>
2020-06-26 01:33:49 -0300

    Implementando componente service con cliente HTTP RestTemplate

--- src/main/java/com/sarracent/servicio/item/services/ItemServiceImpl.java
+++ src/main/java/com/sarracent/servicio/item/services/ItemServiceImpl.java
@@ -1,11 +1,16 @@
 package com.sarracent.servicio.item.services;
 
 import com.sarracent.servicio.item.models.Item;
+import com.sarracent.servicio.item.models.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class ItemServiceImpl implements ItemService {
@@ -15,11 +20,15 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public List<Item> findAll() {
-        return null;
+        List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://locahost:8001/listar", Producto[].class));
+        return productos.stream().map(producto -> new Item(producto, 1)).collect(Collectors.toList());
     }
 
     @Override
     public Item findById(Long id, Integer cantidad) {
-        return null;
+        Map<String, String> pathVariables = new HashMap<String, String>();
+        pathVariables.put("id", id.toString());
+        Producto producto = clienteRest.getForObject("http://locahost:8001/ver/{id}", Producto.class, pathVariables);
+        return new Item(producto, cantidad);
     }
 }
